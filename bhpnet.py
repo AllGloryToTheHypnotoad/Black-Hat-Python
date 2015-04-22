3e18839500b584ba1b5f55435abc6cf83f9de4de
Eid010n <wpennell@gmail.com>
2015-04-21 21:16:09 -0400

    still fleshing this out

--- bhpnet.py
+++ bhpnet.py
@@ -42,5 +42,31 @@ def main():
     if not len(sys.argv[1:]): # if there is more than one element in the array
         usage()
     
+    # read the commandline options    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu",["help","listen","execute","target","port","command","upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o,a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+
+
                                     
 
