2b19941f7847cfbfaaf2b4bc93c26194b71dff3b
Eid010n <wpennell@gmail.com>
2015-04-23 17:20:25 -0400

    fleshing out bhpnet, still bugs in server_loop

--- bhpnet.py
+++ bhpnet.py
@@ -32,29 +32,36 @@ def usage ():
 
 def client_sender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+    print "client object built"
+
     try:
         # connect to our target host
         client.connect((target,port))
         
         if len(buffer):
             client.send(buffer)
+            print "sent!"
             
         while True:
             # now wait for data back
-            recv_len = len(data)
-            response += data
+            recv_len = 1
+            response = ""
             
-            if recv_len < 4096:
-                break
-        print response,
+            while recv_len:
+                data = client.recv(1024)          
+                recv_len = len(data)
+                response += data
+            
+                if recv_len < 4096:
+                    break
+            print response,
         
-        # wait for more input
-        buffer = raw_input("")
-        buffer += "\n"
+            # wait for more input
+            buffer = raw_input("")
+            buffer += "\n"
         
-        # send it off
-        client.send(buffer)
+            # send it off
+            client.send(buffer)
     
     except:
         print "[*] Exception! Exiting."
@@ -63,6 +70,94 @@ def client_sender(buffer):
         client.close()
         
         
+def server_loop():
+    global target
+    
+    # if no target is defined, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+    server.listen(5)
+    
+    while True:
+        client_socket, addr = server.accept()
+        
+        # spin off a new thread to handle the client
+        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+        client_thread.start()
+        
+def run_command(command):
+    # trim the newline
+    command = command.rstrip()
+    
+    # run the command and get the output back
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+    except:
+        output = "[-] Failed to execute command\r\n"
+        
+    # send the output back to the client
+    return output
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+    
+    # check for upload
+    if len(upload_destination):
+        
+        # read in all the bytes of the file and write to our destination
+        
+        file_buffer = ""
+        
+        # keep reading data until none is available
+        while True:
+            data = client_socket.recv(1024)
+            
+            if not data:
+                break
+            else:
+                file_buffer += data
+        
+        # now we take these bytes and write them out
+        try: 
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            
+            # acknowledge that we wrote the file out
+            client_socket.send("Successfully saved file to %s/r/n" % upload_destination)
+        except:
+            client_socket.send("Failed to save file to %s/r/n" % upload_destination)
+            
+    # check for command execution
+    if len(execute):
+        
+        # run the command
+        output = run_command(execute)
+        
+        client.socket.send(output)
+        
+    # now we go into another loop if a command shell was requested
+    
+    if command :
+        while True:
+            # show a simple prompt
+            client_socket.send("<BHP:#> ")
+            
+            # now we revieve until we see a linefeed (enter key)
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            
+            # we have a valid command so execute it and send back the results
+            response = run_command(cmd_buffer)
+            
+            # send the response back to the client
+            client_socket.send(response)
 
 def main():
     # needed to modify global copies of variables
@@ -108,8 +203,10 @@ def main():
             # this will block, so send CTRL-D if not sending input
             # to stdin
             
+            print "reading from command line"
             buffer = sys.stdin.read()
             
+            print "sending data"
             # send data off
             client_sender(buffer)
             
